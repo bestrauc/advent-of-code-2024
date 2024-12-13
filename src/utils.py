7fd1b34f903f985fee4f6e41a27ff0e6e22b69b2
Benjamin Strauch <6814712+bestrauc@users.noreply.github.com>
2024-12-13 22:53:50 +0100

    Add priority queue

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,6 @@
 import re
+import itertools
+import heapq
 
 
 def split_list_at(l: list, pat: str) -> list[list]:
@@ -33,3 +35,37 @@ def transpose(l: list[list]) -> list[list]:
 
 def print_grid(grid: list[list[str]]):
     print("\n".join("".join(row) for row in grid))
+
+
+class PriorityQueue:
+    """A wrapper around the queue implementation suggested by the heapq docs."""
+
+    def __init__(self):
+        self.pq = []  # list of entries arranged in a heap
+        self.entry_finder = {}  # mapping of tasks to entries
+        self.REMOVED = "<removed-task>"  # placeholder for a removed task
+        self.counter = itertools.count()  # unique sequence count
+
+    def add_task(self, task, priority=0):
+        "Add a new task or update the priority of an existing task"
+        if task in self.entry_finder:
+            self.remove_task(task)
+        count = next(self.counter)
+        entry = [priority, count, task]
+        self.entry_finder[task] = entry
+        heapq.heappush(self.pq, entry)
+
+    def remove_task(self, task):
+        "Mark an existing task as REMOVED.  Raise KeyError if not found."
+        entry = self.entry_finder.pop(task)
+        entry[-1] = self.REMOVED
+
+    def pop_task(self):
+        "Remove and return the lowest priority task. Raise KeyError if empty."
+        while self.pq:
+            priority, count, task = heapq.heappop(self.pq)
+            if task is not self.REMOVED:
+                del self.entry_finder[task]
+                return task
+
+        raise KeyError("pop from an empty priority queue")
