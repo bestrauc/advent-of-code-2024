666dadef0eab1f72b2a3cfb07a8843092c05ddbe
Benjamin Strauch <benjamin.strauch@vara.ai>
2024-12-18 07:53:56 +0100

    add template for new days

---
+++ src/day.py
@@ -0,0 +1,12 @@
+"""Template for day X."""
+
+import utils
+
+
+def main():
+    puzzle_input = utils.read_example_input("""""")
+    puzzle_input = utils.read_puzzle_input("inputs/dayX.txt")
+
+
+if __name__ == "__main__":
+    main()
