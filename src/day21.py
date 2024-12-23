02dd8b856a610fdd7a3ff805da86046df8ced94c
Benjamin Strauch <6814712+bestrauc@users.noreply.github.com>
2024-12-23 21:07:11 +0100

    Hurrayyyyy, day 21!!!!!

--- src/day21.py
+++ src/day21.py
@@ -1,17 +1,64 @@
+"""This is a bit of a mess, but eventually I did it.
+
+The gist of it is that I iterate overall possible input patterns for a given
+abstraction level, for instance for the first level:
+
+382A can be input via these strokes, separated per character:
+    ['^', 'A'], ['^', '^', '<', 'A'], ['v', 'v', 'A'], ['v', '>', 'A']
+    ['^', 'A'], ['^', '^', '<', 'A'], ['v', 'v', 'A'], ['>', 'v', 'A']
+    ['^', 'A'], ['<', '^', '^', 'A'], ['v', 'v', 'A'], ['v', '>', 'A']
+    ['^', 'A'], ['<', '^', '^', 'A'], ['v', 'v', 'A'], ['>', 'v', 'A']
+
+I do not just find one shortest path, but all equivalent shortest ones, 
+which makes reconstructing the paths a bit of a mess. However, this could
+easily be precomputed once for both keypads.
+
+Note that I use Dijkstra to find these patterns, which is possibly overkill,
+but there I could easily exclude patterns like ^>^>, which would just cause
+unnecessary turns, which cause excessive inputs on higher level keypads.
+
+I don't think this optimization is really necessary once you get the iteration
+order and the caching right, but I did it while I was exploring things.
+
+So I have a generator for these inputs and iterate over them like thus:
+
+- ['^', 'A']
+    - ['^', '^', '<', 'A']
+        - ['v', 'v', 'A']
+            - ['v', '>', 'A']
+            - ['>', 'v', 'A']
+    - ['<', '^', '^', 'A']
+        - ['v', 'v', 'A']
+            - ['v', '>', 'A']
+            - ['>', 'v', 'A']
+
+And for each level, I recurse into the keypads to find the shortest input
+sequence for them. So runtime-wise, we definitely have to walk this graph
+once, but note for example that we can cache the lower levels: The pattern
+['v', '>', 'A'] occurs twice at the bottom, so we only have to compute its
+shortest keypad-length once and the same for higher-up nodes, etc.
+
+Initially I tried to generate the actual shortest sequence and measure its
+lenght, but that was way too slow and not cacheable - the solution is a
+few billion characters long, after all and we have to cache a few options.
+In the end, just caching the lengths of the subproblems was enough, however.
+"""
+
 import utils
 from collections import defaultdict
+import functools
 
-NUMBER_KEYPAD = [
-    ["7", "8", "9"],
-    ["4", "5", "6"],
-    ["1", "2", "3"],
-    [" ", "0", "A"],
-]
+NUMBER_KEYPAD = (
+    ("7", "8", "9"),
+    ("4", "5", "6"),
+    ("1", "2", "3"),
+    (" ", "0", "A"),
+)
 
-ARROW_KEYPAD = [
-    [" ", "^", "A"],
-    ["<", "v", ">"],
-]
+ARROW_KEYPAD = (
+    (" ", "^", "A"),
+    ("<", "v", ">"),
+)
 
 
 def main():
@@ -26,94 +73,70 @@ def main():
 
     score_sum = 0
     for sequence in puzzle_input:
-        min_numpad = float("inf")
-        min_keypad1 = float("inf")
-        min_keypad2 = float("inf")
-
-        min_score = float("inf")
-        min_seq = None
-
         code_value = utils.nums(sequence)[0]
-        possible_numpad_moves = get_move_sequence(NUMBER_KEYPAD, input_sequence=sequence)
-        for numpad_moves in possible_numpad_moves:
-            moves_on_keypad(NUMBER_KEYPAD, numpad_moves)
-            if len(numpad_moves) > min_numpad:
-                continue
-            min_numpad = min(min_numpad, len(numpad_moves))
+        min_move_len = min_moves(keypad=NUMBER_KEYPAD, sequence=tuple(sequence), depth=26)
 
-            possible_keypad1_moves = get_move_sequence(ARROW_KEYPAD, input_sequence=numpad_moves)
-            for keypad1_moves in possible_keypad1_moves:
-                if len(keypad1_moves) > min_keypad1:
-                    continue
-                min_keypad1 = min(min_keypad1, len(keypad1_moves))
-
-                possible_keypad2_moves = get_move_sequence(ARROW_KEYPAD, input_sequence=keypad1_moves)
-                for keypad2_moves in possible_keypad2_moves:
-                    print("".join(keypad2_moves))
-                    if len(keypad2_moves) > min_keypad2:
-                        continue
-                    min_keypad2 = min(min_keypad2, len(keypad2_moves))
-
-                    score = len(keypad2_moves) * code_value
-                    if min_score > score:
-                        min_score = score
-                        min_seq = keypad2_moves
-
-        print(f"{sequence}: {''.join(min_seq)} (len={len(min_seq)}, score={min_score})")
-        score_sum += min_score
+        score = min_move_len * code_value
+        score_sum += score
 
     print(score_sum)
 
 
-def moves_on_keypad(keypad: list[list[str]], moves: list[str]) -> list[str]:
+@functools.cache
+def min_moves(keypad: tuple[tuple[str]], sequence: tuple[str], depth: int = 0) -> int:
+    if depth == 0:
+        return len(sequence)
+
+    possible_moves_tree = get_move_tree(keypad=keypad, sequence=sequence)
+
+    min_seq_len = None
+    for possible_moves in possible_moves_tree:
+        ongoing_seq_len = 0
+        for move in possible_moves:
+            min_move_len = min_moves(keypad=ARROW_KEYPAD, sequence=tuple(move), depth=depth - 1)
+            ongoing_seq_len += min_move_len
+
+        if min_seq_len is None or ongoing_seq_len < min_seq_len:
+            min_seq_len = ongoing_seq_len
+
+    return min_seq_len
+
+
+def get_move_tree(keypad: tuple[tuple[str]], sequence: tuple[str], last_button: str = "A"):
+    """Recursive way of generating all the ways you can input the sequence."""
+    if len(sequence) == 0:
+        yield []
+        return
+
     h, w = utils.input_dim(keypad)
     button_pos = {keypad[i][j]: (i, j) for i in range(h) for j in range(w)}
 
-    buttons_input = []
-    bi, bj = button_pos["A"]
-    for c in moves:
-        if c == "A":
-            buttons_input.append(keypad[bi][bj])
-            continue
+    next_button = sequence[0]
+    dists1, previous1 = shortest_path(start=(*button_pos[last_button], "|"), grid=keypad)
+    dists2, previous2 = shortest_path(start=(*button_pos[last_button], "-"), grid=keypad)
 
-        (di, dj) = utils.MOVE_TO_DX[c]
-        bi, bj = bi + di, bj + dj
-        assert keypad[bi][bj] != " "
+    # Because my shortest path algorithm punishes turns, we have to find the possible
+    # shortest paths by checking out all the horizontal/vertical starts and endings.
+    min_dist = min(
+        dists1[(*button_pos[next_button], "|")],
+        dists1[(*button_pos[next_button], "-")],
+        dists2[(*button_pos[next_button], "|")],
+        dists2[(*button_pos[next_button], "-")],
+    )
 
-    return buttons_input
-
-
-def get_move_sequence(keypad: list[list[str]], input_sequence: list[str]) -> list[str]:
-    h, w = utils.input_dim(keypad)
-    button_pos = {keypad[i][j]: (i, j) for i in range(h) for j in range(w)}
-
-    total_moves = [[]]
-
-    last_button = "A"
-    for next_button in input_sequence:
-        _, previous = shortest_path(start=button_pos[last_button], grid=keypad)
-        moves = get_moves_rec(previous, target=button_pos[next_button])
-        moves = [m + ["A"] for m in moves]
-
-        total_moves = [t + m for t in total_moves for m in moves]
-
-        last_button = next_button
-
-    return total_moves
-
-
-def get_moves(previous: dict, target: tuple[int, int]) -> list[str]:
     moves = []
+    if min_dist == dists1[(*button_pos[next_button], "|")]:
+        moves += get_moves_rec(previous1, target=(*button_pos[next_button], "|"))
+    if min_dist == dists1[(*button_pos[next_button], "-")]:
+        moves += get_moves_rec(previous1, target=(*button_pos[next_button], "-"))
+    if min_dist == dists2[(*button_pos[next_button], "|")]:
+        moves += get_moves_rec(previous2, target=(*button_pos[next_button], "|"))
+    if min_dist == dists2[(*button_pos[next_button], "-")]:
+        moves += get_moves_rec(previous2, target=(*button_pos[next_button], "-"))
 
-    node = target
-    while previous[node] is not None:
-        (i, j) = node
-        (pi, pj) = previous[node]
-        moves.append(utils.DX_TO_MOVE[(i - pi, j - pj)])
-
-        node = previous[node]
-
-    return list(reversed(moves))
+    # Combine the various ways we can get this char with the ways we can generate the remaining ones.
+    moves = [m + ["A"] for m in moves]
+    yield from ([m] + t for m in moves for t in get_move_tree(keypad, last_button=next_button, sequence=sequence[1:]))
 
 
 def get_moves_rec(previous: dict, target: tuple[int, int]) -> list[str]:
@@ -122,17 +145,29 @@ def get_moves_rec(previous: dict, target: tuple[int, int]) -> list[str]:
 
     all_moves = []
 
-    (ti, tj) = target
+    (ti, tj, t_axis) = target
     for prev in previous[target]:
-        (pi, pj) = prev
-        move_char = utils.DX_TO_MOVE[(ti - pi, tj - pj)]
-        for prev_move in get_moves_rec(previous, target=prev):
-            all_moves.append(prev_move + [move_char])
+        (pi, pj, p_axis) = prev
+
+        # Don't add a movement for rotations.
+        if (ti == pi) and (tj == pj):
+            for prev_move in get_moves_rec(previous, target=prev):
+                all_moves.append(prev_move)
+        else:
+            move_char = utils.DX_TO_MOVE[(ti - pi, tj - pj)]
+            for prev_move in get_moves_rec(previous, target=prev):
+                all_moves.append(prev_move + [move_char])
 
     return all_moves
 
 
-def shortest_path(start: tuple[int, int], grid: list[list[str]], level: int = 0) -> tuple[dict, dict]:
+AXIS_NEIGHBORS = {
+    "|": [(-1, 0), (1, 0)],
+    "-": [(0, -1), (0, 1)],
+}
+
+
+def shortest_path(start: tuple[int, int], grid: list[list[str]]) -> tuple[dict, dict]:
     h, w = utils.input_dim(grid)
 
     q = utils.PriorityQueue()
@@ -146,22 +181,27 @@ def shortest_path(start: tuple[int, int], grid: list[list[str]], level: int = 0)
 
     while len(q.pq) > 0:
         node = q.pop_task()
-        (i, j) = node
+        (i, j, axis) = node
 
-        adj_neighbors = [
-            (i + di, j + dj)
-            for (di, dj) in utils.ADJ4
+        # We can continue in either direction on our axis.
+        neighbors = [((i + di, j + dj, axis), 1) for (di, dj) in AXIS_NEIGHBORS[axis]]
+
+        # Or we rotate, at a high cost.
+        neighbors += [((i, j, "|" if axis == "-" else "-"), 1000)]
+
+        neighbors = [
+            ((ni, nj, axis), cost)
+            for ((ni, nj, axis), cost) in neighbors
             # A valid neighbor is on the keypad and isn't the empty button, which we shouldn't use.
-            if (i + di) in range(h) and (j + dj) in range(w) and grid[i + di][j + dj] != " "
+            if ni in range(h) and nj in range(w) and grid[ni][nj] != " "
         ]
-        neighbors = [(ni, nj) for (ni, nj) in adj_neighbors]
 
-        for neighbor in neighbors:
-            if distances[neighbor] > distances[node] + 1:
+        for neighbor, cost in neighbors:
+            if distances[neighbor] > distances[node] + cost:
                 previous[neighbor] = {node}
-                distances[neighbor] = distances[node] + 1
+                distances[neighbor] = distances[node] + cost
                 q.add_task(neighbor)
-            elif distances[neighbor] == distances[node] + 1:
+            elif distances[neighbor] == distances[node] + cost:
                 previous[neighbor] |= {node}
 
     return distances, previous
